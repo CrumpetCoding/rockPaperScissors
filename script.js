7ff68a3d11d07610f38b9ac91d15fbe73acbe302
CrumpetCoding <124200115+CrumpetCoding@users.noreply.github.com>
2025-05-20 15:45:25 +0100

    Added playRound function

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
 function getComputerChoice(computerChoice){
     var choices = ['rock', 'paper', 'scissors']
     computerChoice = Math.floor(Math.random() * choices.length);
-    console.log(choices[computerChoice])
     return choices[computerChoice];
 }
 
 function getHumanChoice(humanChoice){
-    let choice = prompt("Rock, paper, scissors?: ")
-    console.log(choice);
+    humanChoice = prompt("Rock, paper, scissors?: ")
+    return humanChoice;
 }
 
-//getHumanChoice()//
-//getComputerChoice()//
+function playRound(computerChoice, humanChoice){
+    computerSelection = getComputerChoice()
+    humanSelection = getHumanChoice()
+    console.log("Computer: "+ computerChoice);
+    console.log("Player: " + humanChoice);
 
+
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+playRound();
 
 
 
