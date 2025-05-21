e6cb1c839dbad7938d3f481dd00385d370b28446
CrumpetCoding <124200115+CrumpetCoding@users.noreply.github.com>
2025-05-21 09:45:34 +0100

    Created gameplay conditions for a single round.

--- script.js
+++ script.js
@@ -1,27 +1,97 @@
 function getComputerChoice(computerChoice){
-    var choices = ['rock', 'paper', 'scissors']
+    const choices = ['rock', 'paper', 'scissors']
     computerChoice = Math.floor(Math.random() * choices.length);
     return choices[computerChoice];
 }
 
 function getHumanChoice(humanChoice){
-    humanChoice = prompt("Rock, paper, scissors?: ")
+    
+    const choices = ['rock', 'paper', 'scissors']
+    humanChoice = prompt("Rock, paper, scissors?: ").toLowerCase();
+
     return humanChoice;
 }
 
 function playRound(computerChoice, humanChoice){
-    computerSelection = getComputerChoice()
-    humanSelection = getHumanChoice()
+    computerChoice = getComputerChoice()
+    humanChoice = getHumanChoice()
     console.log("Computer: "+ computerChoice);
     console.log("Player: " + humanChoice);
+
+    switch (humanChoice){
+        case "rock":
+            if (computerChoice === "rock")
+            {
+                console.log("It's a draw!");
+            }
+            else if (computerChoice === "paper")
+            {
+                computerScore++;
+                console.log("You lose. Paper beats rock.");
+            }
+            else
+            {
+                humanScore++;
+                console.log("You win! Rock beats scissors.");
+            }
+            break;
+
+        case "paper":
+            if (computerChoice === "rock")
+            {
+                humanScore++;
+                console.log("You win! Paper beats rock.");
+            }
+            else if (computerChoice === "paper")
+            {
+                console.log("It's a draw!")
+            }
+            else
+            {
+                computerScore++;
+                console.log("You lose. Scissors beat paper.");
+            }
+            break;
+        
+        case "scissors":
+            {
+            if (computerChoice === "rock")
+            {
+                computerScore++;
+                console.log("You lose. Rock beats scissors.");
+            }
+            else if (computerChoice === "paper")
+            {
+                humanScore++;
+                console.log("You win! Scissors beat paper.")
+            }
+            else
+            {
+                console.log("It's a draw!");
+            }
+            break;
+
+        default:
+            console.log("ERROR Invalid input. Please try again.");
+        
+            }
+    }
+
 
 
 }
 
-let humanScore = 0;
-let computerScore = 0;
+function playGame(){
+    let humanScore = 0;
+    let computerScore = 0;
+    
+    playRound();
+}
 
-playRound();
+playGame();
+
+
+
 
 
 
